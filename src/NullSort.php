0871dfa8a64bf7dcbc37164eecd9f76ca2be3f33
viktor <oksentiuk.viktor@gmail.com>
2019-03-02 22:46:52 +0200

    add null

---
+++ src/NullSort.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: morakot
+ * Date: 02.03.19
+ * Time: 22:45
+ */
+
+namespace App;
+
+
+class NullSort implements StrategyInterface
+{
+public function sortAlgoritm(array $data): array
+{
+    
+}
+}
